d827862882a3a97a5158a1f04e72645df48aaecb
Philippe <philippe.idalgoprestes@gmail.com>
2024-07-17 14:23:28 -0300

    new map and some content

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let countries = [["Brasil", "66%", "34%"],
-                ["França", "37.5%", "48%"]]
+let countries = [["Brasil", "65%", "30%"],
+                ["França", "25.5%", "47%"]]
 let earth = document.getElementById("earthTerrain")
 for(let i = 0; i < countries.length; i++){
     let c = countries[i];
@@ -18,11 +18,11 @@ function sleep(ms) {
 async function moveToCountry(e){
     let divToGo = document.getElementById(e.target.title);
     divToGo.scrollIntoView({behavior: "smooth"})
-    await sleep(500)
+    openCountryContent(e.target.title)
+    await sleep(200)
     divToGo.style.border = "3px solid rgb(0, 150, 255)";
     await sleep(1000)
     divToGo.style.border = "3px solid rgb(255, 255, 255)";
-    openCountryContent(e.target.title)
 }
 
 function openCountryContent(id){
